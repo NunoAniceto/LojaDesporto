cf1407af5e70a08ca360ab0693214d1c4b3e6708
Nuno Aniceto <anicetonuno74@gmail.com>
2021-01-05 17:48:00 +0000

    alteraçoes

--- LojaDesporto/Models/Produto.cs
+++ LojaDesporto/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,15 @@ namespace LojaDesporto.Models
 {
     public class Produto
     {
+        
         public int ProdutoId { get; set; }
+
+        [Required(ErrorMessage ="Deve preencher o nome")]
+        [StringLength(200,MinimumLength =4, ErrorMessage ="O nome deve ter pelo menos 4 carateres e nao deve exceder os 200 carateres")]
         public string Nome { get; set; }
         public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Deve preencher o preço")]
         public decimal Preco { get; set; }
     }
 }
